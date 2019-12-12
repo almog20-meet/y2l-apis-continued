3f97702c3202b65a8bb1bd6dd8465fb8df061634
myname21-meet <myname21@meet.mit.edu>
2019-12-12 11:46:29 -0500

    lab

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+headers = {'Authorization': '13a54025dcfe4cc4862e0f30a5cffc88'}
+
+# this is the url of where your request will go
+api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -15,6 +22,19 @@ def study_image():
     # example we covered in the slides! 
 
     # YOUR CODE HERE!
+    data ={"inputs": [
+      {
+        "data": {
+          "image": {
+            "url": "https://samples.clarifai.com/metro-north.jpg"
+          }
+        }
+      }
+    ]}
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
+
     
     return render_template('home.html', results="No results yet :(")
 
